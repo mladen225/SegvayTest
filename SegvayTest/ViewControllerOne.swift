b3f1e581d1067848daa2bcfdeaed53d00e3498d0
codepool <codepool@Macbooks-MacBook-Pro.local>
2021-06-16 16:43:35 +0200

    first commit

---
+++ SegvayTest/ViewControllerOne.swift
@@ -0,0 +1,19 @@
+//
+//  ViewControllerOne.swift
+//  SegvayTest
+//
+//  Created by codepool on 3. 3. 2021..
+//
+
+import UIKit
+
+class ViewControllerOne: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SegvayTest/ViewControllerTwo.swift
@@ -0,0 +1,23 @@
+//
+//  ViewControllerTwo.swift
+//  SegvayTest
+//
+//  Created by codepool on 3. 3. 2021..
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+}
